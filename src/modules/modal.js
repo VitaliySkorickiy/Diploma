27eee5985123bf2410d528634227f92d9ab4f22e
zarias2020 <zarias@yandex.ru>
2021-05-14 17:54:25 +0300

    блок не нашли услугу, вызов мод окна

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -2,7 +2,8 @@ const modal = () => {
   const modalCallback = document.querySelector('.modal-callback'),
     modalOverlay = document.querySelector('.modal-overlay'),
     callbackBtn = document.querySelector('.callbackBtn'),
-    modalClose = modalCallback.querySelector('.modal-close');
+    modalClose = modalCallback.querySelector('.modal-close'),
+    buttonServices = document.querySelector('.button-services');
 
   const openClose = (btn, modal, background, display) => {
     btn.addEventListener('click', () => {
@@ -10,6 +11,7 @@ const modal = () => {
       background.style.display = display;
     });
   };
+  openClose(buttonServices, modalCallback, modalOverlay, 'block');
   openClose(callbackBtn, modalCallback, modalOverlay, 'block');
   openClose(modalClose, modalCallback, modalOverlay, 'none');
   openClose(modalOverlay, modalCallback, modalOverlay, 'none');
